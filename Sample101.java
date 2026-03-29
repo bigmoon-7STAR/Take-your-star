569ceb62158a1a5269526a364ab630cf7ae43828
bigmoon-7STAR <example.poll-5x@icloud.com>
2026-03-29 21:25:38 +0900

    Update Sample101.java

--- Sample101.java
+++ Sample101.java
@@ -4,8 +4,8 @@ public class sample101{
   /*
   *Jacaを使った最も基本的なプログラム
   */
-  public static void main(String[] args) (
+  public static void main(String[] args) {
   System.out.println("Hello,workd.");  //「Hello,world.」という文字列を表示します。
-  )
+  }
   
 }
